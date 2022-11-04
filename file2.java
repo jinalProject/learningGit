24f8a1a00e15f8cf5f2324c0ed94a58c0ec617b5
Jinal Patel <jinalcode@gmail.com>
2022-11-04 11:50:27 +0530

    file2 added sum of array and printed

--- file2.java
+++ file2.java
@@ -1,5 +1,11 @@
 public class file2 {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+
+        for (int x : arr) {
+            sum += x;
+        }
+        System.out.println(sum);
     }
 }
